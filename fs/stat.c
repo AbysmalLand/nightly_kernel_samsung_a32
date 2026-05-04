898b14b42ab6f4e8d429b065801168bff6fbc728
mertsan2007 <110729187+mertsan2007@users.noreply.github.com>
2026-05-04 20:04:11 +0100

    update scope minimized ksu hooks to 2.0

--- fs/stat.c
+++ fs/stat.c
@@ -375,8 +375,7 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	return cp_new_stat(&stat, statbuf);
 }
 #endif
-
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_KSUD)
 extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
 #if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
 extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr); // for 32-bit
@@ -390,8 +389,7 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
-
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_KSUD)
 	ksu_handle_newfstat_ret(&fd, &statbuf);
 #endif
 	return error;
@@ -513,8 +511,7 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
-
-#ifdef CONFIG_KSU // for 32-bit
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_KSUD) // for 32-bit
 	ksu_handle_fstat64_ret(&fd, &statbuf);
 #endif
 	return error;
